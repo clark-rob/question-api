90e4f343c8d7ae6d950cb8bd1836234364d58c54
clark-rob <clark.rob.g@gmail.com>
2019-03-20 10:53:03 -0400

    Curl Scripts Created
    - Delete, update, show, index created and tested

---
+++ curl-scripts/situations/update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+curl "http://localhost:4741/situations/${ID}" \
+  --include \
+  --request PATCH \
+  --header "Content-Type: application/json" \
+  --data '{
+    "situation": {
+      "name": "'"${NAME}"'",
+      "question": "'"${QUESTION}"'"
+    }
+  }'
+
+echo
